45ad58bc79e66ca0cc2fad625be5e91262d6a4c8
Fox <Fox@WIN-CR1AB8M9SVC>
2016-06-25 18:29:32 +0300

    Add project files.

---
+++ producersConsumersCollections/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace producersConsumersCollections
+{
+    public class Class1
+    {
+    }
+}
